855c30ae0e6f80f59c48cce8db2b00bf4e311aeb
Nahyun-K <rnjsskgus990313@gmail.com>
2023-07-11 03:33:45 +0900

    Player class 생성 (Create Player class)

---
+++ League/src/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+    private final int playerNumber;
+    private final String position;
+
+    public Player(int playerNumber, String position) {
+        this.playerNumber = playerNumber;
+        this.position = position;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
